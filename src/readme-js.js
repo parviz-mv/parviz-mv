4313c046d53fda15432f99a50cea1e2e743ae57a
Parviz-Makhkamov <parvizjon555@gmail.com>
2023-04-23 07:34:25 +0500

    Create readme-js.js

---
+++ src/readme-js.js
@@ -0,0 +1,33 @@
+
+document.write("Hello World");
+
+var status = 
+{ 
+  learning : [Python, Go],
+  interests: [Research, Open Source]
+}
+
+const skills = 
+{
+  languages :[Javascript, Golang,Python],
+  
+  frameworks:[Nodejs, ExpressJs],
+  
+  libraries: [ReactJs]
+  
+  databases :[MySql, PostgreSQL, Firebase],
+  
+  platforms :[GCP, AWS, Firebase, Heroku, Pythonanywhere]
+}
+
+
+Developer dev = new Developer(parviz-makhkamov);
+
+while(dev.isCoding() && dev.isStuck())  
+{
+  dev.openBrowser();
+  dev.openStackOverFlow();
+  dev.pasteCodeInIde();
+  dev.feelProfessional();
+}
+
